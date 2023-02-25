b889d3893dc252f0b82655d4c82faccdff16618b
Ihor Chubov <ihor.chub@gmail.com>
2023-02-25 15:53:45 +0200

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+// import { useState, useEffect } from 'react';
+// import { nanoid } from 'nanoid';
 import { GlobalStyle } from './GlobalStyles';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 export const App = () => {
   return (
