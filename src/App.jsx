8d8fd27b3948976fba074f0277b39ba5922ccad8
Ihor Chubov <ihor.chub@gmail.com>
2023-02-25 15:48:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { GlobalStyle } from './GlobalStyles';
+import styled from 'styled-components';
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <p>calculator</p>
+      </div>
+      <GlobalStyle />
+    </>
+  );
+};
